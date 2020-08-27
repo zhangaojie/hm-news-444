23f6255ff4a264364aebb24b0a2f3dc87f8979ca
杰哥哥 <85723773@qq.com>
2020-08-27 17:13:12 +0800

    登录页面和注册页面完成

--- src/main.js
+++ src/main.js
@@ -1,7 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+// 导入通用的样式
+import './styles/base.less'
+import './styles/iconfont.css'
+import Vant from 'vant'
+import 'vant/lib/index.css'
+import 'amfe-flexible'
+import HmHeader from './components/HmHeader.vue'
+import HmLogo from './components/HmLogo.vue'
+import axios from 'axios'
 
+// 把axios挂载到vue的原型
+Vue.prototype.$axios = axios
+// 给axios配置默认的baseURL ,基准地址
+axios.defaults.baseURL = 'http://localhost:3000'
+
+Vue.component('hm-header', HmHeader)
+Vue.component('hm-logo', HmLogo)
+
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
